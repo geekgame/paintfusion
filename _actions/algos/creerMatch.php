e26a527cb0cd07c95b96e1debf82d8ab2c2747d1
geekgame <matthieu.vancayzeele@ig2i.fr>
2015-06-21 15:38:07 +0200

    index admin
    page index redirection vers accueil_admin.php

--- _actions/algos/creerMatch.php
+++ _actions/algos/creerMatch.php
@@ -4,6 +4,8 @@ include("../../_utils/connect.php");
 
 session_start();
 
+
+
 //On commence par prendre les deux premières équipes du premier tour
 
 #region premierTour
---
+++ _admin/index.php
@@ -0,0 +1,4 @@
+<?php
+
+header("location:accueil_admin.php");
+die();
